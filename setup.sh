4667bd63cbb4cf0959d0dd3fc2e5f5e522ea2212
Jonathan Rahn <rahn.jonathan@gmail.com>
2024-08-09 20:26:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@ cd llm.c
 git checkout -q 6e6a528111cc6641f09d0ebf2ca1e7432d1c87a4
 cd ..
 
-pip install chess datasets tqdm
-
 cp -r scripts/ llm.c/scripts/
 cp -r dev/ llm.c/
+pip install -r requirements.txt
 
-cd llm.c/dev/
+cd llm.c
+pip install -r requirements.txt
+
+cd dev/
 bash download_starter_pack.sh
