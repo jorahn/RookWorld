6d04ffbaa8a200bb15d891ba450cc5c994217879
Jonathan Rahn <rahn.jonathan@gmail.com>
2024-08-09 20:59:33 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 cd llm.c/dev/data/rook
-#python generate_lichess.py -p $STOCKFISH_PATH
-#python generate_selfplay.py -p $STOCKFISH_PATH
+python generate_lichess.py -p $STOCKFISH_PATH
+python generate_selfplay.py -p $STOCKFISH_PATH
 cd ..
 
-#python rook.py
+python rook.py
 cd ../..
 
-#bash scripts/run_gpt2_124M_rook.sh
+bash scripts/run_gpt2_124M_rook.sh
 
 cd dev/eval
-python export_hf.py --input ../../log_gpt2_124M_rook/model_00002508.bin --output ../../log_gpt2_124M_rook/rook_gpt2_124M_hf
+python export_hf.py \
+    --input ../../log_gpt2_124M_rook/model_00002508.bin \ 
+    --output ../../log_gpt2_124M_rook/rook_gpt2_124M_hf
 
-python rook_selfplay.py --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/
+python rook_selfplay.py \ 
+    --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/
