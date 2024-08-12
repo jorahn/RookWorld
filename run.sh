f3e0563b24bf9f9721ef089a505bfcb38e78f598
Jonathan Rahn <rahn.jonathan@gmail.com>
2024-08-12 09:43:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ bash scripts/run_gpt2_124M_rook.sh
 cd dev/eval
 python export_hf.py --input ../../log_gpt2_124M_rook/model_00005000.bin --output ../../log_gpt2_124M_rook/rook_gpt2_124M_hf
 
-python rook_selfplay.py -g --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/
-
 python rook_accuracy.py -g --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/ -d ../data/rook/rook_val_500.txt.bak
+
+python rook_selfplay.py --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/
