fd8925117859435b5c374b7df93fd2b184952e79
jorahn <rahn.jonathan@gmail.com>
2024-08-13 08:25:34 +0200

    add rook vs. stockfish eval to run.sh example

--- run.sh
+++ run.sh
@@ -11,6 +11,11 @@ bash scripts/run_gpt2_124M_rook.sh
 cd dev/eval
 python export_hf.py --input ../../log_gpt2_124M_rook/model_00005000.bin --output ../../log_gpt2_124M_rook/rook_gpt2_124M_hf
 
+# evaluate the model on the validation set (legal move %, best move accuracy, top 5 move accuracy)
 python rook_accuracy.py -g --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/ -d ../data/rook/rook_val_500.txt.bak
 
+# play 50 games with greedy decoding vs stockfish at strength level 0 and 50ms time limit per move
+python rook_vs_stockfish.py -n 50 --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/ -g -p $STOCKFISH_PATH -s 0 -l 0.05
+
+# play 50 games against itself with sampling (topk=5, temp=0.6) until illegal move or win/loss/draw outcome
 python rook_selfplay.py --model_path ../../log_gpt2_124M_rook/rook_gpt2_124M_hf/
