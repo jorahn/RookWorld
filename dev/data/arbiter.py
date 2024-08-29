b1a6f20f11c688e721831d16d79c6f6e6c6cdfd2
jorahn <rahn.jonathan@gmail.com>
2024-08-29 20:41:08 +0200

    arbiter first try

---
+++ dev/data/arbiter.py
@@ -0,0 +1,92 @@
+"""
+ROOK dataset
+
+example doc to highlight the structure of the dataset (newline delimited text file):
+
+P: r1b1kbnr/pp2pppp/n1p5/q2PP3/3P4/2N5/PP3PPP/R1BQKBNR b KQkq - 2 6                          M: c6d5 a6b4 c8d7 a6c7 a6b8      E: -2.0 -2.07 -2.92 -2.79 -2.66            B: c8d7
+"""
+
+import os, argparse, random
+import multiprocessing as mp
+from glob import glob
+
+import numpy as np
+import tiktoken
+from tqdm import tqdm
+from datasets import load_dataset
+
+from data_common import write_datafile
+# ------------------------------------------
+
+parser = argparse.ArgumentParser(description="ARBITER dataset preprocessing")
+parser.add_argument("-i", "--input", type=str, default="jrahn/arbiter_2m", help="ARBITER dataset version txt-files")
+parser.add_argument("-s", "--seed", type=int, default=42, help="Random seed")
+args = parser.parse_args()
+
+
+random.seed(args.seed)
+shard_size = 2**24 
+# for now only small datasets
+# originally reduced to create a suitable small validation set, maybe increase back to 2**20
+name = "arbiter"
+local_dir = "arbiter"
+DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+ds = load_dataset(args.input)
+print(f"{len(ds['train']):,} train samples, {len(ds['test']):,} test samples")
+
+
+# init the tokenizer
+enc = tiktoken.get_encoding("gpt2")
+eot = enc._special_tokens['<|endoftext|>'] # end of text token
+def tokenize(doc):
+    # tokenizes a single document and returns a numpy array of uint16 tokens
+    tokens = [eot] # the special <|endoftext|> token delimits all documents
+    tokens.extend(enc.encode_ordinary(doc["text"]))
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16)
+    return tokens_np_uint16
+
+def process_dataset(ds, split):
+    # tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
+    nprocs = max(1, os.cpu_count() - 2) # don't hog the entire system
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        # preallocate buffer to hold current shard
+        all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, ds, chunksize=16):
+
+            # is there enough space in the current shard for the new tokens?
+            if token_count + len(tokens) < shard_size:
+                # simply append tokens to current shard
+                all_tokens_np[token_count:token_count+len(tokens)] = tokens
+                token_count += len(tokens)
+                # update progress bar
+                if progress_bar is None:
+                    progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+                progress_bar.update(len(tokens))
+            else:
+                # write the current shard and start a new one
+                filename = os.path.join(DATA_CACHE_DIR, f"{name}_{split}_{shard_index:06d}.bin")
+                # split the document into whatever fits in this shard; the remainder goes to next one
+                remainder = shard_size - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+                write_datafile(filename, all_tokens_np)
+                shard_index += 1
+                progress_bar = None
+                # populate the next shard with the leftovers of the current doc
+                all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
+                token_count = len(tokens)-remainder
+
+        # write any remaining tokens as the last shard
+        if token_count != 0:
+            filename = os.path.join(DATA_CACHE_DIR, f"{name}_{split}_{shard_index:06d}.bin")
+            write_datafile(filename, all_tokens_np[:token_count])
+
+process_dataset(ds["train"], "train")
+process_dataset(ds["test"], "val")
---
+++ scripts/run_gpt2_124M_rook_5m_3e_lr1e4.sh
@@ -0,0 +1,42 @@
+# GPT-2 (124M) repro on FineWeb
+# 124M parameter model on 10B tokens
+# => 6 * 124e6 * 10e9 = 7.44e18 ~= 7e18 capability model
+# 18,865 steps of 524,288 tokens/step
+# on 8X A100 80GB SXM ($14/hr) steps in ~300ms/iter
+# => training time 18,865 * 300ms = 94.3 min ~= $20
+
+make train_gpt2cu USE_CUDNN=1
+out_dir="log_gpt2_124M_rook_5m_3e_lr1e4"
+done_file="$out_dir/DONE_000034932"
+
+# in case the training stalls or crashes, loop to resume (-y 1)
+while true; do
+
+    # exit condition is that optimization has finished
+    if [ -f "$done_file" ]; then
+        echo "File $done_file exists. Exiting the loop."
+        break
+    fi
+
+    # run python dev/data/cheessreason.py to prepro data
+    mpirun -np 2 ./train_gpt2cu \
+                -i "dev/data/rook/rook_train_*.bin" \
+                -j "dev/data/rook/rook_val_*.bin" \
+                -o $out_dir \
+                -v 100 -s 10000 -g 144 \
+                -n 10000 \
+                -h 0 \
+                -b 32 -t 1024 \
+                -d 65536 \
+                -r 0 \
+                -z 0 \
+                -c 0.1 \
+                -l 0.0001 \
+                -q 0.0 \
+                -u 500 \
+                -y 1 \
+                -x 34932 \
+                -e "d12"
+
+    sleep 1
+done
